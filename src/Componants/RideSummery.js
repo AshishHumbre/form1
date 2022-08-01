1292b403962fe5a16db465f159137b11c99b66bc
ASHish <humbreashish@gmail.com>
2022-08-01 22:05:48 +0530

    commit location

---
+++ src/Componants/RideSummery.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import React, { Component } from 'react'
+import './RideSummery.css'
+
+
+function RideSummery() {
+    return (
+      <>
+      
+      
+      
+      </>
+      
+    );
+  }
+  
+  export default RideSummery;
+  
